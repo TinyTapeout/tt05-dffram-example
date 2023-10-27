bcee4b7c336031fc8a60bef30c0ea8ca1ab4c59f
Uri Shaked <uri@urishaked.com>
2023-10-28 00:33:20 +0300

    fix: add missing power strips
    fixes LVS and IR Drop report issues

--- odb_power.py
+++ odb_power.py
@@ -26,8 +26,10 @@ def power(reader):
   met4 = tech.findLayer('met4')
   vpwr_wire = vpwr_net.getSWires()[0]
   vgnd_wire = vgnd_net.getSWires()[0]
-  odb.dbSBox_create(vpwr_wire, met4, 28280, 143520, 29880, 144120, "STRIPE")
-  odb.dbSBox_create(vgnd_wire, met4, 105080, 143520, 106680, 144120, "STRIPE")
+  for i in range(3):
+    odb.dbSBox_create(vpwr_wire, met4, 28280 + i * 153600, 11880, 29880 + i * 153600, 144120, "STRIPE")
+  for i in range(2):
+    odb.dbSBox_create(vgnd_wire, met4, 105080 + i * 153600, 11880, 106680 + i * 153600, 144120, "STRIPE")
 
 if __name__ == "__main__":
   power()
